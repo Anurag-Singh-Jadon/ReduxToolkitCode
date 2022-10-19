3d9b592157de587e183b89b6bba442b9878093fe
anuredapple2014 <anurag@redappletech.com>
2022-10-19 18:29:26 +0530

    commit 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,25 @@
 import { useSelector, useDispatch } from "react-redux";
+import {counterActions} from '../store/index'
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
 
   const counter = useSelector(
-    (state) => state.counter
+    (state) => state.counter.counter
   ); /* It return a State Function   */
-  const show = useSelector((state) => state.showCounter);
+  const show = useSelector((state) => state.counter.showCounter);
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));   //{type: SOME_UNIQUE_IDENTIFIER,payload: 10}
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -31,41 +31,49 @@
 
 // This Hiding Code means I am showing Toggle functionality in down and If we want normal functionality we can use upper code
 
-
-const initialState = {counter: 0, showCounter: true}
-
-
-const counterReducer = (state = initialState,action) =>{
- if(action.type === 'increment'){
-    return{
-        counter: state.counter + 1,
-        showCounter: state.showCounter
-    };
- }
-
- if(action.type === 'increase'){
-    return{
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter
+import {createSlice,configureStore} from '@reduxjs/toolkit';  //We can use createReducer or createSlice to initialize value      
+const initialCounterState = {counter: 0, showCounter: true}
+const counterSlice = createSlice({
+    name:'counter',
+    initialState:initialCounterState,
+    reducers:{
+        
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state,action) {
+            state.counter = state.counter + action.payload
+        },
+        toggleCounter(state){
+            state.showCounter = !state.showCounter;
+        }
     }
- }
- if(action.type === 'decrement'){
-    return{
-        counter: state.counter - 1,
-        showCounter: state.showCounter
-    };
- }
+});
 
- if(action.type === 'toggle'){
-    return{
-        showCounter: !state.showCounter,
-        counter: state.counter
-    };
- }
-
- return state;
+const initialAuthState =  {
+    isAuthenticated: false
 };
+const authSlice = createSlice({
+    name:'authentication',
+    initialState:initialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated = true;
+        },
+        logout(state){
+            state.isAuthenticated = false;
+        }
+    }
+});
 
-const store = createStore(counterReducer);
 
+
+const store = configureStore({
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer},
+});
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
